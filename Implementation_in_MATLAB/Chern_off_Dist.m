275a22597ce97e8fc763ae9cf95b8052d69652ba
Ioannis Sorokos <yanos2soros@gmail.com>
2020-05-03 20:32:57 +0200

    Update Chern_off_Dist.m
    Updated reference, clarified operations

--- Implementation_in_MATLAB/Chern_off_Dist.m
+++ Implementation_in_MATLAB/Chern_off_Dist.m
@@ -1,8 +1,10 @@
-% TODO: Provide formula and reference
-function Dist = Chern_off_Dist(D2,D1) % @Koorosh: Why are the inputs reversed ?
+% @return Dist : the distance between two probability density functions D1 and D2
+% From: (Listing 3.149 and 3.150) in Page 98 - Fukunaga, K. (1992). Introduction to Statistical Pattern Recognition (Second Edition). Academic Press.
+function Dist = Chern_off_Dist(D1, D2)
+    % Listing 3.150 of reference
     %alpha = 0.5; %% Locking alpha to 0.5 effectively uses the Bhattacharrya measure instead
-    D1(~any(~isnan(D1), 2),:)=[]; % @Koorosh: Remove NaN from D1 ?
-    D2(~any(~isnan(D2), 2),:)=[]; % @Koorosh: Remove NaN from D2 ?
+    D1(~any(~isnan(D1), 2),:)=[]; % Remove NaN from D1 
+    D2(~any(~isnan(D2), 2),:)=[]; % Remove NaN from D2
     mean1 = mean(D1);
     mean2 = mean(D2);
     cov1 = cov(D1);
@@ -11,5 +13,5 @@ function Dist = Chern_off_Dist(D2,D1) % @Koorosh: Why are the inputs reversed ?
     var_avg = (cov1 + cov2) * 0.5;
     ln_coeff = 0.5 * log(det(var_avg) / sqrt(det(cov1) * det(cov2)));
     dist2 = 0.125 * (mean_diff * inv(var_avg)) * mean_diff' + ln_coeff;
-    Dist = 1 - exp(-dist2);
+    Dist = 1 - exp(-dist2); % Listing 3.149 of reference, taking the complement of the upper bound of the Bayes error
 end
